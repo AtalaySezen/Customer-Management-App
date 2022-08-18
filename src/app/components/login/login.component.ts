0b9d442f34009ceef567acab7999919470d0b80e
AtalaySezen <atalaysezen@yahoo.com.tr>
2022-08-19 00:40:02 +0300

    login component reactive form validation

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -14,27 +14,15 @@ export class LoginComponent implements OnInit {
   ngOnInit(): void {
   }
 
-  // formCard = new FormGroup({
-  //   email:new FormControl('',[Validators.minLength(2),Validators.email,Validators.required]),
-  //   password:new FormControl('',[Validators.minLength(5),Validators.required])
-  // })
-
   email: string = '';
   password: string = '';
-  
   formCard = new FormGroup({
-    username:new FormControl('username',[Validators.minLength(5),Validators.email]),
-    surname:new FormControl('surname',[Validators.minLength(5)])
+    username: new FormControl('username', [Validators.minLength(5), Validators.email]),
+    password: new FormControl('surname', [Validators.minLength(5)])
   })
 
 
   login() {
-    if (this.email == '' && this.password == '') {
-      alert("hata")
-      return;
-    }
-
-
     this.auth.login(this.email, this.password);
     this.email = '';
     this.password = '';
