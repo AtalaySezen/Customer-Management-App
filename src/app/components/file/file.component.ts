2fad0102ad09241659751269a1c0e2b7b62505b9
AtalaySezen <atalaysezen@yahoo.com.tr>
2022-08-23 13:23:46 +0300

    File Upload Feature added.

--- src/app/components/file/file.component.ts
+++ src/app/components/file/file.component.ts
@@ -1,4 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { AngularFireStorage } from '@angular/fire/compat/storage';
+import { FileMetaData } from 'src/app/model/file-meta-data';
+import { FileService } from 'src/app/shared/file.service';
+import { DataService } from 'src/app/shared/data.service';
+import { finalize } from 'rxjs/operators';
+import { empty } from 'rxjs';
 
 @Component({
   selector: 'app-file',
@@ -7,9 +13,69 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FileComponent implements OnInit {
 
-  constructor() { }
+  selectedFiles !: FileList;
+  currentFileUpload !: FileMetaData;
+  percentage: number = 0;
+
+  listOfFiles : FileMetaData[] = [];
+
+
+  constructor(private fileService: FileService, private fireStorage: AngularFireStorage, private dataService : DataService) { }
 
   ngOnInit(): void {
+    this.getAllFiles();
+  }
+
+  selectFile(event: any) {
+    this.selectedFiles = event.target.files;
+  }
+
+  uploadFile() {
+     this.currentFileUpload =  new FileMetaData(this.selectedFiles[0]);
+     const path = 'Uploads/'+this.currentFileUpload.file.name;
+     const storageRef = this.fireStorage.ref(path);
+     const uploadTask = storageRef.put(this.selectedFiles[0]);
+
+     uploadTask.snapshotChanges().pipe(finalize( () => {
+        storageRef.getDownloadURL().subscribe(downloadLink => {
+          this.currentFileUpload.id = '';
+          this.currentFileUpload.url = downloadLink;
+          this.currentFileUpload.size = this.currentFileUpload.file.size;
+          this.currentFileUpload.name = this.currentFileUpload.file.name;
+
+          this.fileService.saveMetaDataOfFile(this.currentFileUpload);
+        })
+        this.ngOnInit();
+     })
+     ).subscribe( (res : any) => {
+        this.percentage = (res.bytesTransferred * 100 / res.totalBytes);
+     }, err => {
+        console.log('Error occured');
+     });
+
+  }
+  showSpinner=true;
+  getAllFiles() {
+    this.fileService.getAllFiles().subscribe( res => {
+        this.listOfFiles = res.map((e : any) => {
+            const data = e.payload.doc.data();
+            data.id = e.payload.doc.id;
+            //console.log(data);
+            this.showSpinner = false;
+            return data;
+        });
+    }, err => {
+        console.log('Error occured while fetching file meta data');
+    })
+  }
+
+  deleteFile(file : FileMetaData) {
+
+    if(window.confirm('Are you sure you want to delete '+file.name   + '?')) {
+      this.fileService.deleteFile(file);
+      this.ngOnInit();
+   }
+    
   }
 
 }
